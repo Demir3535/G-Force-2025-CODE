cff9fcdca174d76233774396ca03253112509e4e
Demir <demiravci35l@gmail.com>
2025-02-21 13:43:40 +0300

    updated shooter

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -1,94 +1,78 @@
 package frc.robot.subsystems.shooter;
-
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotConstants.ShooterConstants;
 import frc.robot.RobotConstants.PortConstants.CAN;
-import com.revrobotics.spark.SparkBase.ResetMode;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-
 public class ShooterSubsystem extends SubsystemBase {
-    SparkMax shooterMotor1;
-    SparkMaxConfig shooterMotor1Config;
-    static SparkClosedLoopController shooterMotor1Controller;
+    private SparkMax shooterMotor;
     private DigitalInput limitSwitch;
     private boolean isShooterRunning = false;
     private boolean gameElementDetected = false;
     private PWMSparkMax ledController;
     private static final double RED = 0.61;
     private static final double GREEN = 0.77;
-
+    
+    private boolean readyToShoot = false; // Is game piece ready?
     public ShooterSubsystem() {
-        limitSwitch = new DigitalInput(ShooterConstants.LIMIT_PORT); // Updated name to reflect limit switch
-        shooterMotor1 = new SparkMax(CAN.SHOOTER_MOTOR_1, MotorType.kBrushless);
-        shooterMotor1Controller = shooterMotor1.getClosedLoopController();
+        limitSwitch = new DigitalInput(ShooterConstants.LIMIT_PORT);
+        shooterMotor = new SparkMax(CAN.SHOOTER_MOTOR_1, MotorType.kBrushless);
         ledController = new PWMSparkMax(0);
-
-        // Motor configurations
-        shooterMotor1Config = new SparkMaxConfig();
-        /** shooterMotor1Config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder);
-        shooterMotor1Config.closedLoop.maxMotion.maxVelocity(ShooterConstants.MAX_MOTOR_RPM);
-        shooterMotor1Config.closedLoop.maxMotion.maxAcceleration(ShooterConstants.MAX_MOTOR_ACCELERATION);
-        shooterMotor1Config.closedLoop.pid(.5, 0.0, 0.0); */
     }
-
     @Override
     public void periodic() {
-        // Update game element detection status
-        gameElementDetected = !limitSwitch.get(); // Inverted for normally open switch
-
-        // When game element is detected
-        if (gameElementDetected) {
+        // Check limit switch status
+        gameElementDetected = !limitSwitch.get();
+        
+        // If game piece is detected and motor is in intake mode, stop the motor
+        if (gameElementDetected && isShooterRunning && !readyToShoot) {
             stopShooter();
+            readyToShoot = true; // Ready to shoot
         }
-
         updateLEDStatus();
-
         // Debug information
         SmartDashboard.putBoolean("Limit Switch Status", limitSwitch.get());
         SmartDashboard.putBoolean("Is Shooter Running", isShooterRunning);
         SmartDashboard.putBoolean("Game Element Detected", gameElementDetected);
+        SmartDashboard.putBoolean("Ready To Shoot", readyToShoot);
     }
-
     private void updateLEDStatus() {
-        if (gameElementDetected) {
-            ledController.set(GREEN); // Game element ready
+        if (readyToShoot) {
+            ledController.set(GREEN); // Ready to shoot
         } else {
-            ledController.set(RED); // Waiting for game element
+            ledController.set(RED); // Waiting for game piece
         }
     }
-
     public void shooterButton() {
-        if (!gameElementDetected) {
-            // If no game element and motor is not running, start motor
-            moveAtSpeed(0.5); // Collect at half speed
-        } else {
-            // If motor is already running, stop it
+        if (!gameElementDetected && !isShooterRunning) {
+            // If no game piece and motor is stopped -> Start intake
+            moveAtSpeed(0.5); // Half power for intake 
+            readyToShoot = false;
+        }
+        else if (readyToShoot && gameElementDetected) {
+            // If game piece present and ready to shoot -> Shoot
+            moveAtSpeed(1.0); // Full power for shooting
+            readyToShoot = false;
+        }
+        else if (isShooterRunning) {
+            // If motor is running -> Stop
             stopShooter();
         }
     }
-
     public void moveAtSpeed(double speed) {
-        shooterMotor1.set(speed);
+        shooterMotor.set(speed);
         isShooterRunning = true;
     }
-
     public void stopShooter() {
-        shooterMotor1.set(0);
+        shooterMotor.set(0);
         isShooterRunning = false;
     }
-
     public boolean isShooterRunning() {
         return isShooterRunning;
     }
-
     public boolean hasGameElement() {
         return gameElementDetected;
     }
